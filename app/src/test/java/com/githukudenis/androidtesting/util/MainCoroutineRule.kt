75e0597a6c95d165a389a71a2c0f81e9981a50cd
Denis Githuku <githukudenis@gmail.com>
2022-05-02 22:17:55 +0300

    Test for viewmodels passed

---
+++ app/src/test/java/com/githukudenis/androidtesting/util/MainCoroutineRule.kt
@@ -0,0 +1,25 @@
+package com.githukudenis.androidtesting.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+): TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
